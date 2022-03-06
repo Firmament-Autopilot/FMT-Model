fc5827cc29708a83d628e959dd383eb4983f9d52
jiachi <jiachi.zou@gmail.com>
2022-03-06 14:23:34 +0800

    Implement mission mode

---
+++ bus/enum/NAV_Cmd.m
@@ -0,0 +1,42 @@
+classdef NAV_Cmd < Simulink.IntEnumType
+    
+    enumeration
+        None(0)
+        Waypoint(16)
+        Return(20)
+        Land(21)
+        Takeoff(22)
+    end
+  
+    methods (Static)
+
+        function defaultValue = getDefaultValue()
+            % GETDEFAULTVALUE  Returns the default enumerated value.
+            %   If this method is not defined, the first enumeration is used.
+            defaultValue = NAV_Cmd.None;
+        end
+
+        function dScope = getDataScope()
+            % GETDATASCOPE  Specifies whether the data type definition should be imported from,
+            %               or exported to, a header file during code generation.
+            dScope = 'Auto';
+        end
+
+        function desc = getDescription()
+            % GETDESCRIPTION  Returns a description of the enumeration.
+            desc = 'enumeration of navigation command';
+        end
+
+        function fileName = getHeaderFile()
+            % GETHEADERFILE  Returns path to header file if non-empty.
+            fileName = '';
+        end
+
+        function flag = addClassNameToEnumNames()
+            % ADDCLASSNAMETOENUMNAMES  Indicate whether code generator applies the class name as a prefix
+            %                          to the enumeration.
+            flag = true;
+        end
+
+    end
+end 
