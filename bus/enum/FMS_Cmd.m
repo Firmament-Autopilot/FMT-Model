4f05e850dfb90968cbcec65bd5363016da4512a0
jiachi <jiachi.zou@gmail.com>
2022-03-27 17:14:45 +0800

    rename fms command

--- bus/enum/FMS_Cmd.m
+++ bus/enum/FMS_Cmd.m
@@ -1,13 +1,45 @@
 classdef FMS_Cmd < Simulink.IntEnumType
-  enumeration
-    CMD_None(0)
-    CMD_PreArm(1000)
-    CMD_Arm(1001)
-    CMD_Disarm(1002)
-    CMD_Takeoff(1003)
-    CMD_Land(1004)
-    CMD_Return(1005)
-    CMD_Pause(1006)
-    CMD_Continue(1007)
-  end
+    enumeration
+        None(0)
+        PreArm(1000)
+        Arm(1001)
+        Disarm(1002)
+        Takeoff(1003)
+        Land(1004)
+        Return(1005)
+        Pause(1006)
+        Continue(1007)
+    end
+  
+    methods (Static)
+
+        function defaultValue = getDefaultValue()
+            % GETDEFAULTVALUE  Returns the default enumerated value.
+            %   If this method is not defined, the first enumeration is used.
+            defaultValue = FMS_Cmd.None;
+        end
+
+        function dScope = getDataScope()
+            % GETDATASCOPE  Specifies whether the data type definition should be imported from,
+            %               or exported to, a header file during code generation.
+            dScope = 'Auto';
+        end
+
+        function desc = getDescription()
+            % GETDESCRIPTION  Returns a description of the enumeration.
+            desc = 'enumeration of FMS command';
+        end
+
+        function fileName = getHeaderFile()
+            % GETHEADERFILE  Returns path to header file if non-empty.
+            fileName = '';
+        end
+
+        function flag = addClassNameToEnumNames()
+            % ADDCLASSNAMETOENUMNAMES  Indicate whether code generator applies the class name as a prefix
+            %                          to the enumeration.
+            flag = true;
+        end
+
+    end
 end 
