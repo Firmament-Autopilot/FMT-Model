f61a249c8fd8cc0eb90f40f1415b739c1a378763
JCZou <jiachi.zou@gmail.com>
2022-08-06 07:46:59 +0800

    Add FW Controller and FMS

---
+++ model/Controller/FW-Controller/controller_model_init.m
@@ -0,0 +1,61 @@
+model_version = 'v0.0.1';
+model_name = 'FW Controller';
+
+%% load configuration
+load('control_default_config.mat');
+
+%% Constant Variable
+CONTROL_CONST.dt = 0.002;   % model execution period
+
+%% Exported Value
+CONTROL_EXPORT_VALUE.period = uint32(CONTROL_CONST.dt*1e3);
+CONTROL_EXPORT_VALUE.model_info = int8([model_name, ' ', model_version, 0]); % 0 for end of string
+% Export to firmware
+CONTROL_EXPORT = Simulink.Parameter(CONTROL_EXPORT_VALUE);
+CONTROL_EXPORT.CoderInfo.StorageClass = 'ExportedGlobal';
+
+%% Paramaters
+CONTROL_PARAM_VALUE.ROLL_P = single(7);
+CONTROL_PARAM_VALUE.PITCH_P = single(7);
+CONTROL_PARAM_VALUE.ROLL_PITCH_CMD_LIM = single(pi/3);
+
+CONTROL_PARAM_VALUE.ROLL_RATE_P = single(0.1);
+CONTROL_PARAM_VALUE.PITCH_RATE_P = single(0.2);
+CONTROL_PARAM_VALUE.YAW_RATE_P = single(0.15);
+CONTROL_PARAM_VALUE.ROLL_RATE_I = single(0.1);
+CONTROL_PARAM_VALUE.PITCH_RATE_I = single(0.1);
+CONTROL_PARAM_VALUE.YAW_RATE_I = single(0.2);
+CONTROL_PARAM_VALUE.RATE_I_MIN = single(-0.1);
+CONTROL_PARAM_VALUE.RATE_I_MAX = single(0.1);
+CONTROL_PARAM_VALUE.P_Q_CMD_LIM = single(pi/2);
+CONTROL_PARAM_VALUE.R_CMD_LIM = single(pi);
+
+CONTROL_PARAM_VALUE.FW_AIRSPEED_TRIM = single(13);
+CONTROL_PARAM_VALUE.FW_FF = single(0.2);
+CONTROL_PARAM_VALUE.FW_FF_LIMIT = single(0.3);
+CONTROL_PARAM_VALUE.FW_PI_LIMIT = single(1);
+CONTROL_PARAM_VALUE.FW_ROLL_EFFC = single(-1);
+CONTROL_PARAM_VALUE.FW_PITCH_EFFC = single(-1);
+CONTROL_PARAM_VALUE.FW_YAW_EFFC = single(1);
+
+CONTROL_PARAM_VALUE.FW_TECS_PITCH_F = single(0.1);
+CONTROL_PARAM_VALUE.FW_TECS_THOR_FF = single(0.1);
+CONTROL_PARAM_VALUE.FW_TECS_PITCH_P = single(0.1);
+CONTROL_PARAM_VALUE.FW_TECS_THOR_P = single(0.1);
+CONTROL_PARAM_VALUE.FW_TECS_PITCH_I = single(0.1);
+CONTROL_PARAM_VALUE.FW_TECS_THOR_I = single(0.1);
+CONTROL_PARAM_VALUE.FW_TECS_PITCH_D = single(0);
+CONTROL_PARAM_VALUE.FW_TECS_THOR_D = single(0);
+CONTROL_PARAM_VALUE.FW_TECS_RATIO = single(1);
+CONTROL_PARAM_VALUE.FW_TECS_SWITCH = single(2);
+CONTROL_PARAM_VALUE.FW_TECS_ANSW = single(1);
+CONTROL_PARAM_VALUE.FW_TECS_VERR_K = single(1);
+CONTROL_PARAM_VALUE.FW_TECS_ANG_K = single(1);
+CONTROL_PARAM_VALUE.FW_TECS_W2T = single(1);
+CONTROL_PARAM_VALUE.FW_TECS_U2T = single(1);
+CONTROL_PARAM_VALUE.FW_TECS_R2P = single(0.2);
+CONTROL_PARAM_VALUE.FW_TECS_R2T = single(0.2);
+
+% Export to firmware
+CONTROL_PARAM = Simulink.Parameter(CONTROL_PARAM_VALUE);
+CONTROL_PARAM.CoderInfo.StorageClass = 'ExportedGlobal';
---
+++ model/FMS/FW-FMS/fms_model_init.m
@@ -0,0 +1,46 @@
+model_version = 'v0.0.1';
+model_name = 'FW FMS';
+
+%% load model configuration
+load('fms_default_config.mat');
+
+%% Constant Variable
+FMS_CONST.dt = 0.004;
+
+%% Exported Value 
+FMS_EXPORT_VALUE.period = uint32(FMS_CONST.dt*1e3);
+FMS_EXPORT_VALUE.model_info = int8([model_name, ' ', model_version, 0]); % 0 for end of string
+
+FMS_EXPORT = Simulink.Parameter(FMS_EXPORT_VALUE);
+FMS_EXPORT.CoderInfo.StorageClass = 'ExportedGlobal';
+
+%% Paramaters
+FMS_PARAM_VALUE.THROTTLE_DZ = single(0.15);
+FMS_PARAM_VALUE.YAW_DZ = single(0.15);
+FMS_PARAM_VALUE.ROLL_DZ = single(0.1);
+FMS_PARAM_VALUE.PITCH_DZ = single(0.1);
+FMS_PARAM_VALUE.XY_P = single(0.95);
+FMS_PARAM_VALUE.Z_P = single(1);
+FMS_PARAM_VALUE.VEL_Z_LIM = single(2.5);
+FMS_PARAM_VALUE.YAW_P = single(2.5);
+FMS_PARAM_VALUE.YAW_RATE_LIM = single(pi/3);
+FMS_PARAM_VALUE.ROLL_PITCH_LIM = single(pi/6);
+FMS_PARAM_VALUE.L1 = single(10);
+FMS_PARAM_VALUE.CRUISE_SPEED = single(13);
+FMS_PARAM_VALUE.TAKEOFF_H = single(1.5);
+FMS_PARAM_VALUE.ACCEPT_R = single(0.5);
+
+FMS_PARAM_VALUE.Y_P = single(0.95);
+FMS_PARAM_VALUE.ACC_Y_LIM = single(5);
+FMS_PARAM_VALUE.ROLL_LIM = single(pi/4);
+FMS_PARAM_VALUE.PITCH_LIM = single(pi/4);
+FMS_PARAM_VALUE.FW_AIRSPD_MAX = single(30.0);
+
+FMS_PARAM_VALUE.DISARM_OUT = single([1000 1000 1500 1500 1500 1500, zeros(1, 10)]);
+FMS_PARAM_VALUE.STANDBY_OUT = single([1000 1000 1500 1500 1500 1500, zeros(1, 10)]);
+
+%TODO, remove it
+FMS_PARAM_VALUE.VEL_XY_LIM = single(5);
+
+FMS_PARAM = Simulink.Parameter(FMS_PARAM_VALUE);
+FMS_PARAM.CoderInfo.StorageClass = 'ExportedGlobal';
