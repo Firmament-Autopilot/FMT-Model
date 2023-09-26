9239f8ff9e5f6d514346df0fda993cefc64a9787
Josh Zou <zoujiachi666@163.com>
2023-09-26 18:50:23 +0800

    Fix version issue for mlog parser

--- utils/log_parser/functions/mlog_parser.m
+++ utils/log_parser/functions/mlog_parser.m
@@ -62,7 +62,6 @@ LogHeader.num_param_group = fread(fileID, 1, 'uint8=>uint8');
 for n = 1:LogHeader.num_param_group
     LogHeader.param_group_list(n).name = fread(fileID, [1 LogHeader.max_name_len], 'uint8=>char');
     LogHeader.param_group_list(n).num_param = fread(fileID, 1, 'uint32=>uint32');
-%     LogHeader.param_group_list(n).name = LogHeader.param_group_list(n).name(~isspace(LogHeader.param_group_list(n).name));
     LogHeader.param_group_list(n).name = deblank(LogHeader.param_group_list(n).name);
     
     for k = 1:LogHeader.param_group_list(n).num_param
@@ -70,7 +69,6 @@ for n = 1:LogHeader.num_param_group
         LogHeader.param_group_list(n).param(k).type = fread(fileID, 1, 'uint8=>uint8');
         index = LogHeader.param_group_list(n).param(k).type+1;
         LogHeader.param_group_list(n).param(k).val = fread(fileID, 1, TYPE_CAST(index));
-%         LogHeader.param_group_list(n).param(k).name = LogHeader.param_group_list(n).param(k).name(~isspace(LogHeader.param_group_list(n).param(k).name));
         LogHeader.param_group_list(n).param(k).name = deblank(LogHeader.param_group_list(n).param(k).name);
     end
 end
@@ -161,9 +159,7 @@ fprintf('\nLog parse finish!\n');
 % fprintf('log elapsed time: %f(s)\n', )
 for n = 1:LogHeader.num_bus
     % remove 0
-%     BusName = strrep(LogHeader.bus(n).name, char(0), '');
     BusName = deblank(LogHeader.bus(n).name);
-%     BusName = BusName(~isspace(BusName));
 	fprintf('%s: %d msg recorded\n', BusName, MsgCount(n));
 end
 
@@ -182,15 +178,11 @@ for n = 1:LogHeader.num_bus
        continue; 
     end
     
-%     BusName = strrep(LogHeader.bus(n).name, '"', '');
-%     BusName = BusName(~isspace(BusName));
     BusName = deblank(LogHeader.bus(n).name);
     
     % find timestamp
     timestamp_id = 0;
     for k = 1:LogHeader.bus(n).num_elem
-%         ElemName = strrep(LogHeader.bus(n).elem_list(k).name, '"', '');
-%         ElemName = ElemName(~isspace(ElemName));
         ElemName = deblank(LogHeader.bus(n).elem_list(k).name);
         if strcmp(ElemName, "timestamp_ms") || strcmp(ElemName, "timestamp")
             timestamp_id = k;
@@ -206,8 +198,6 @@ for n = 1:LogHeader.num_bus
     
     % construct Bus variable
     for k = 1:LogHeader.bus(n).num_elem
-%         ElemName = strrep(LogHeader.bus(n).elem_list(k).name, '"', '');
-%         ElemName = ElemName(~isspace(ElemName));
         ElemName = deblank(LogHeader.bus(n).elem_list(k).name);
         
         exp = sprintf('timeseries(LogMsg{index}{k}'', time_stamp);');
