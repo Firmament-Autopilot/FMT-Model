3c7d144c30f8a5b49e079e16a61bd2d6262c66ac
Josh Zou <zoujiachi666@163.com>
2025-10-29 17:20:57 +0800

    Optimize DataSIM script to automatically execute load_param and mission_data_preprocess script

--- utils/log_parser/functions/get_param_val.m
+++ utils/log_parser/functions/get_param_val.m
@@ -6,7 +6,26 @@ for i = 1:row
    if strcmp(param_list(i).name, group_name)
       for j = 1:param_list(i).num_param
           if strcmp(param_list(i).param(j).name, param_name)
-              val = param_list(i).param(j).val;
+              switch param_list(i).param(j).type
+                  case 0
+                      val = int8(param_list(i).param(j).val);
+                  case 1
+                      val = uint8(param_list(i).param(j).val);
+                  case 2
+                      val = int16(param_list(i).param(j).val);
+                  case 3
+                      val = uint16(param_list(i).param(j).val);
+                  case 4
+                      val = int32(param_list(i).param(j).val);
+                  case 5
+                      val = uint32(param_list(i).param(j).val);
+                  case 6
+                      val = single(param_list(i).param(j).val);
+                  case 7
+                      val = double(param_list(i).param(j).val);
+                  otherwise
+                      val = param_list(i).param(j).val;
+              end
               return;
           end
       end
